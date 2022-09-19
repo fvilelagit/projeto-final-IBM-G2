445bdf757e325331faab5f29b6fc9939fbe51e0f
Vilela <rhcp_fabio@hotmail.com>
2022-09-19 14:58:42 -0300

    JWT Insert

---
+++ src/main/java/ibm/grupo2/helloBank/HelloBankApplication.java
@@ -0,0 +1,22 @@
+package ibm.grupo2.helloBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class HelloBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBankApplication.class, args);
+
+	}
+	@Bean
+	public PasswordEncoder getPassWordEncoder(){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
+}
